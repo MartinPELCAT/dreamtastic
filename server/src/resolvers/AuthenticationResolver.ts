f7da95edec89cf74c7057cd28a24c35cb0db0fa0
MartinPELCAT <martinpelcat@hotmail.com>
2020-10-30 08:55:30 +0100

    install bcryptjs instead of brypt because windows sucks

--- server/src/resolvers/AuthenticationResolver.ts
+++ server/src/resolvers/AuthenticationResolver.ts
@@ -1,4 +1,4 @@
-import { compare, hash } from "bcrypt";
+import { compare, hash } from "bcryptjs";
 import { Arg, Ctx, Mutation, Query, Resolver } from "type-graphql";
 import { Repository } from "typeorm";
 import { InjectRepository } from "typeorm-typedi-extensions";
